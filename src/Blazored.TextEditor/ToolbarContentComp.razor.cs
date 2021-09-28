5d0e433fcc6b6d52965b12f33be3cfbefff25300
somegenericdev <vincenzo.lisi@protonmail.com>
2021-09-28 21:21:08 +0000

    turned ToolbarContent into a component
    TODO: add all Quill's features to the ToolbarContent component. See https://quilljs.com/standalone/full/

---
+++ src/Blazored.TextEditor/ToolbarContentComp.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blazored.TextEditor
+{
+    public partial class ToolbarContentComp
+    {
+        [Parameter] public List<string> Fonts { get; set; }
+        [Parameter] public bool ShowFontControls { get; set; } = true;
+        [Parameter] public bool ShowSizeControls { get; set; }
+        [Parameter] public bool ShowStyleControls { get; set; }
+        [Parameter] public bool ShowColorControls { get; set; }
+        [Parameter] public bool ShowIndentationControls { get; set; }
+        [Parameter] public bool ShowAlignmentControls { get; set; }
+        [Parameter] public bool ShowListControls { get; set; }
+        [Parameter] public bool ShowHypertextLinkControls { get; set; }
+
+
+    }
+}
