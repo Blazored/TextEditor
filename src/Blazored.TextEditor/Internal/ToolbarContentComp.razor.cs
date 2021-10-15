a98ccb4b5a28826c6675c6f03d7ace383ad63b01
somegenericdev <86524896+somegenericdev@users.noreply.github.com>
2021-10-16 00:55:33 +0200

    added an easy way to add Fonts through the Fonts parameter. made the use of the component simpler and more blazor-ish through the Toolbar parameter.

---
+++ src/Blazored.TextEditor/Internal/ToolbarContentComp.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blazored.TextEditor.Internal
+{
+    public partial class ToolbarContentComp
+    {
+        [CascadingParameter] public Toolbar Toolbar { get; set; }
+        [CascadingParameter] public List<string> Fonts { get; set; }
+    }
+}
---
+++ src/Blazored.TextEditor/Toolbar.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blazored.TextEditor
+{
+    public class Toolbar
+    {
+        private bool _showFullToolbar;
+
+        public bool ShowFullToolbar
+        {
+            get { return _showFullToolbar; }
+            set
+            {
+                _showFullToolbar = value;
+                if (value == true)
+                {
+                    SetAllPropertiesToTrue();
+                }
+            }
+        }
+
+        public bool ShowFontControls { get; set; } = true;
+        public bool ShowSizeControls { get; set; }
+        public bool ShowStyleControls { get; set; }
+        public bool ShowColorControls { get; set; }
+        public bool ShowHeaderControls { get; set; }
+        public bool ShowQuotationControls { get; set; }
+        public bool ShowCodeBlockControls { get; set; }
+        public bool ShowListControls { get; set; }
+        public bool ShowIndentationControls { get; set; }
+        public bool ShowAlignmentControls { get; set; }
+        public bool ShowDirectionControls { get; set; }
+        public bool ShowHypertextLinkControls { get; set; }
+        public bool ShowInsertImageControls { get; set; }
+        public bool ShowEmbedVideoControls { get; set; }
+        public bool ShowMathControls { get; set; }
+        public bool ShowCleanFormattingControls { get; set; }
+
+        public void SetAllPropertiesToTrue()
+        {
+            ShowFontControls = true;
+            ShowSizeControls = true;
+            ShowStyleControls = true;
+            ShowColorControls = true;
+            ShowHeaderControls = true;
+            ShowQuotationControls = true;
+            ShowCodeBlockControls = true;
+            ShowAlignmentControls = true;
+            ShowListControls = true;
+            ShowIndentationControls = true;
+            ShowDirectionControls = true;
+            ShowHypertextLinkControls = true;
+            ShowInsertImageControls = true;
+            ShowEmbedVideoControls = true;
+            ShowMathControls = true;
+            ShowCleanFormattingControls = true;
+        }
+
+    }
+}
