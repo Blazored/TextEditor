4f1866d3aa425c7404b5144ec681e43e1ad1f9c3
Steven Luiten <steven.luiten@awisoftware.nl>
2024-12-31 13:41:51 +0100

    Update to .NET 8.0

--- samples/BlazorClientSide/Program.cs
+++ samples/BlazorClientSide/Program.cs
@@ -1,16 +1,7 @@
-﻿using Microsoft.AspNetCore.Blazor.Hosting;
+﻿using BlazorClientSide;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-namespace BlazorClientSide
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("app");
 
-        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
-            BlazorWebAssemblyHost.CreateDefaultBuilder()
-                .UseBlazorStartup<Startup>();
-    }
-}
+await builder.Build().RunAsync();
